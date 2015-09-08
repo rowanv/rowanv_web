dd5d7352634c45ce5cdd167ad315daadc85a5d05
rowanv <rrvspam@gmail.com>
2015-09-08 17:41:00 -0500

    Now serving the visualizations in separate views, also added the static file directory

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+import os
+
+
 
 def portfolio(request):
 	return render(request, 'portfolio.html')
+
+def windchill(request):
+	with open('portfolio/templates/visualizations/windchill_table.html') as myfile:
+		data = "\n".join(line for line in myfile)
+	return HttpResponse(data)
+
+def oecd_unemployment(request):
+	with open('portfolio/templates/visualizations/oecd_unemployment.html') as myfile:
+		data = "\n".join(line for line in myfile)
+	return HttpResponse(data)
--- rowanv_web/urls.py
+++ rowanv_web/urls.py
@@ -13,12 +13,19 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+import os
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 from portfolio import views
 
+static = os.path.join( os.path.dirname(__file__), 'static' )
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^portfolio/', views.portfolio, name='portfolio'),
+    url(r'^portfolio/$', views.portfolio, name='portfolio'),
+    #url(r'^portfolio/windchill/', TemplateView.as_view(template_name='visualizations/windchill_table.html')),
+    url(r'^portfolio/windchill/', views.windchill, name='windchill'),
+    url(r'^portfolio/oecd_unemployment/', views.oecd_unemployment, name='oecd_unemployment'),
 ]
